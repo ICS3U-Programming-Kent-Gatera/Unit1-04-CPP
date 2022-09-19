756f5478c4ed6a4c6c9905afe7edc3381e9029c8
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-09-19 03:42:35 +0000

    This program calculates the area and perimeter of a rectangle in C++

---
+++ areaPerRect.cpp
@@ -0,0 +1,15 @@
+// Copyright [2020] <Mr. Coxall>"  [legal/copyright]
+//
+// Date : 09 - 18 - 22
+// This program calculates the area and perimeter of a rectangle.
+// With dimensions 9 cm by 3 cmD
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions" << std::endl;
+    std::cout << "9 cm by 3 cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: " << (9 * 3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << ((9 + 3) * 2) << "cm" << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// Copyright [2020] <Mr. Coxall>"  [legal/copyright]
+//
+// Date : 09 - 18 - 22
+// This program does basic math.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9+2=" << (9 + 2) << std::endl;
+    std::cout << "9-4=" << (9 - 4) << std::endl;
+    std::cout << "6/3=" << (6 / 3) << std::endl;
+    std::cout << "9+5*2=" << (9 + 5 * 2) << std::endl;
+    std::cout << "3+2/2=" << (3 + 2 / 2) << std::endl;
+    std::cout << "9/3^2=" << (9 / 3 ^ 2) << std::endl;
+    std::cout << "9+pow(2,3)=" << (9 + pow(2, 3)) << std::endl;
+}
